a03309658a3eb7b717ff0fb729e467b34ea5368d
Vaniom <florent.p83@gmail.com>
2018-10-01 11:51:38 +0200

    Code etat initial sur pc desktop à supprimer

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var btnElt = document.getElementById("btn");
 var reponseDiv = document.getElementById("reponse");
 btnElt.preventDefault;
+var typeGen = "";
 var part0Resp = "";
 var part1Resp = "";
 var part2Resp = "";
@@ -12,19 +13,28 @@ var part0 = ['Bélier: ', 'Taureau: ', 'Gémeaux: ', 'Cancer: ', 'Lion: ', 'Vier
 var part1 = ['votre travail ', 'votre santé ', 'votre relation avec les autres ', 'votre situation financière ', 'votre relation amoureuse ', 'votre mental '];
 var part2 = ['tient en ce moment beaucoup de place dans votre vie. ', 'vous préoccupe plus que d\'ordinaire. ', 'est une grande source d\'angoisse. ', 'semble vous avoir abandonné. ', 'sera source de profonde douleur. ', 'est au centre de vos préoccupations. ', 'est au niveau zéro. '];
 var part3 = ['C\'est le moment d\'en changer!', 'Les astres sont de votre côté... Du mauvais côté.', 'Achetez une boîte d\'antidepresseurs.', 'Achetez un ticket de tombola, ou pas.', 'Un Mars et ça repart!', 'Prenez les choses avec joie!', 'Enfin c\'est pas sûr, les planètes se plantent parfois.', 'Surveillez votre consommation d\'alcool.', 'Faites plus de sport.', 'Mangez des fruits et fumez moins.', "N'écoutez pas les infos, c'est mauvais pour vous."];
+//Recuperation du type de generateur choisi
+
 //generation de la phrase au clic sur le bouton
-btnElt.addEventListener("click", function(){// ajout d'un ecouteur d'evenement sur le bouton
+btnElt.addEventListener("click", function(){// ajout d'un ecouteur d'evenement sur le bouton Générer
     nombreLigne = document.getElementById('nombre').value;// choix utilisateur du nombre de lignes à générer stocké dans la variable nombreLigne
+    typeGen = document.getElementById("typeGen").value;// selection du type de genrateur
     reponseDiv.innerHTML = ""; // on efface le contenu de la div
-    for (var i = 0; i < nombreLigne; i++) { // on repete la boucle x fois pour generer x phrases
-        console.log(nombreLigne);
-        part0Resp = part0[Math.floor(Math.random() * part0.length)];// tirage aleatoire d'un element dans chaque tableau
-        part1Resp = part1[Math.floor(Math.random() * part1.length)];
-        part2Resp = part2[Math.floor(Math.random() * part2.length)];
-        part3Resp = part3[Math.floor(Math.random() * part3.length)];
+    if (typeGen == "horoscope"){
+        for (var i = 0; i < nombreLigne; i++) { // on repete la boucle x fois pour generer x phrases        
+            part0Resp = part0[Math.floor(Math.random() * part0.length)];// tirage aleatoire d'un element dans chaque tableau
+            part1Resp = part1[Math.floor(Math.random() * part1.length)];
+            part2Resp = part2[Math.floor(Math.random() * part2.length)];
+            part3Resp = part3[Math.floor(Math.random() * part3.length)];
+            var child = document.createElement("p"); // creation d'un nouveau paragraphe
+            child.textContent = part0Resp + part1Resp + part2Resp + part3Resp; // definition du contenu du paragraphe
+            reponseDiv.appendChild(child); // ajout du paragraphe dans la div        
+        }
+    } else {
         var child = document.createElement("p"); // creation d'un nouveau paragraphe
-        child.textContent = part0Resp + part1Resp + part2Resp + part3Resp; // definition du contenu du paragraphe
-        reponseDiv.appendChild(child); // ajout du paragraphe dans la div
-        //btnElt.style.display = "none";        
+            child.textContent = "En construction: Cette fonctionnalité sera disponible prochainement"; // definition du contenu du paragraphe
+            reponseDiv.appendChild(child); // ajout du paragraphe dans la div     
     }
+    console.log(nombreLigne);
+    console.log(typeGen);
 })
